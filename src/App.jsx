084fe24f9bfc4d5ed1ec07d9ec6c5694574bb184
Georgette Dalen <85463029+grgttdln@users.noreply.github.com>
2023-07-31 18:18:27 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import he from "he";
+import {nanoid} from "nanoid";
+import React from 'react'
+import Quizzical from './Quizzical';
+import Body from './body';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function App() {
+
+  const [questionPool, setQuestionPool] = React.useState({})
+  const [startnewgamebtn, setStartnewgamebtn] = React.useState(1)
+  const [question, setQuestion] = React.useState([])
+  const [start, setStart] = React.useState(false)
+
+
+  React.useEffect(function() {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=hard&type=multiple")
+      .then(res => res.json())
+      .then(data => setQuestionPool(data.results))
+      .catch(error => console.log(error))
+  }, [startnewgamebtn])
+
+
+  React.useEffect(() => {
+    async function fetchQs() {
+      const res = await fetch("https://opentdb.com/api.php?amount=5&difficulty=hard&type=multiple")
+      const data = await res.json()
+      setQuestionPool(data.results)
+    }
+
+    fetchQs()
+  }, [startnewgamebtn])
+  
+
+  function handleStartNewGame() {
+    setStartnewgamebtn(prev => prev + 1)
+    setQuestion([])
+    getQuestionSet()
+  }
+
+  function isStart() {
+    setTimeout(() => {
+      setStart(prev => !prev)
+    }, 3000)
+    getQuestionSet()
+  }
+
+  async function getQuestionSet() {
+    for(let i = 0; i < 5; i++) {
+      let curr = questionPool[i]
+      const choices = [...curr.incorrect_answers, curr.correct_answer].sort(() => .5 - Math.random())
+      setQuestion(prev => {
+        return ([
+          ...prev,
+          {
+            id: nanoid(),
+            category: he.decode(curr.category),
+            tanong: he.decode(curr.question),
+            correctAns: he.decode(curr.correct_answer),
+            questionChoices: choices.map(i => he.decode(i))
+          }]
+        )
+      })
+    }
+  }
+
+
+  return (
+    <>
+      <main>
+        {
+          start === true
+          ?
+          <Body 
+            q={question}
+            handleStartNewGame={handleStartNewGame}
+          />
+          :
+          <>
+          <Quizzical 
+            start={isStart}
+          />
+          </>
+        }
+      </main>
+    </>
+  )
+}
+
---
+++ src/Body.jsx
@@ -0,0 +1,216 @@
+import Confetti from 'react-confetti';
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './Body.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Body(props) {    
+
+  React.useEffect(() => {
+    props.q.map(q => {
+      setCorrectQA(prev => {
+        return {
+          ...prev, 
+          [q.id]: q.correctAns
+        }
+      })
+    })
+  }, [props])
+
+
+  const [selectedAns, setSelectedAns] = React.useState({})
+  const [isSubmitted, setIsSubmitted] = React.useState(false)
+  const [correctQA, setCorrectQA] = React.useState({})
+  const [score, setScore] = React.useState(0)
+
+
+  function ansSelection(obj, id) {
+    if([id] in selectedAns) {
+      setSelectedAns(prev => {
+        return ({
+          ...prev,
+          [id]: obj
+        })
+      })
+    } else {
+      setSelectedAns(prev => {
+        return ({
+          ...prev,
+          [id]: obj
+        })
+      })
+    }
+  }
+
+  function restartVars() {
+    setSelectedAns({})
+    setIsSubmitted(prev => !prev)
+    setCorrectQA({})
+    setScore(0)
+  }
+
+  function checkQuiz() {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    setIsSubmitted(prev => !prev)
+
+    for(const [key, value] of Object.entries(correctQA)) {
+      if(selectedAns[key] === value) {
+        setScore(prev => prev + 1)
+      }
+    }
+  }
+
+  
+  const questionDisplay = props.q.map(q =>
+    <div className='body--container-2' id={q.id}>
+      <Card className='body--card'>
+        <Card.Header className='body--category'>{q.category}</Card.Header>
+        <Card.Body>
+          <Card.Title className='body--tanong'>{q.tanong}</Card.Title>
+          <Card.Text>
+              {
+                // choices are accessible
+                isSubmitted === false
+                ?
+                  q.questionChoices.map(choice =>
+                    Object.values(selectedAns).includes(choice)
+                    ?
+                    <Button
+                            active
+                            onClick={() => ansSelection(choice, q.id)} 
+                            id={q.id}
+                            value={choice}
+                            className="body--btn-choice active"
+                    >
+                      {choice}
+                    </Button> 
+                    :
+                    <Button 
+                            className="body--btn-choice" 
+                            onClick={() => ansSelection(choice, q.id)} 
+                            id={q.id}
+                            value={choice}
+                    >
+                      {choice}
+                  </Button>
+                  )
+                :
+                // choices are disabled
+                    //const [correctQA, setCorrectQA] = React.useState({})
+                    // const [selectedAns, setSelectedAns] = React.useState({})
+                q.questionChoices.map(choice =>
+                  Object.values(selectedAns).includes(choice) 
+                  ?
+                    Object.values(correctQA).includes(choice)
+                    ?
+                    <Button 
+                            variant="success"
+                            onClick={() => ansSelection(choice, q.id)} 
+                            id={q.id}
+                            value={choice}
+                            className="body--btn-choice active"
+                            disabled
+                    >
+                      {choice}
+                    </Button> 
+                    :
+                    <Button 
+                            variant="danger"
+                            onClick={() => ansSelection(choice, q.id)} 
+                            id={q.id}
+                            value={choice}
+                            className="body--btn-choice active"
+                            disabled
+                    >
+                      <s>{choice}</s>
+                    </Button> 
+                  :
+                    Object.values(correctQA).includes(choice)
+                    ?
+                    <Button 
+                            variant="success"
+                            onClick={() => ansSelection(choice, q.id)} 
+                            id={q.id}
+                            value={choice}
+                            className="body--btn-choice active"
+                            disabled
+                    >
+                      {choice}
+                    </Button> 
+                    :
+                    <Button 
+                            onClick={() => ansSelection(choice, q.id)} 
+                            id={q.id}
+                            value={choice}
+                            disabled
+                    >
+                      <s>{choice}</s>
+                    </Button>
+                )
+              }
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+
+  
+  const quizResultDisplay = 
+  <div className='body--card-result-1'>
+    <Confetti />
+    <Card className='body--card-result-2'>
+      <Card.Body>
+        <div className='body--card-result-title'>The Quiz is Finished!</div>
+        <div className='body--card-result-subtitle' onClick={event => {
+            props.handleStartNewGame()
+            restartVars()
+          }
+        }>
+          Correct Answers:
+          <Card className='body--card-score'>{score} / {props.q.length}</Card>
+          <div className='body--card-newgame'>Start New Game</div>
+        </div>
+      </Card.Body>
+    </Card>
+  </div>
+
+
+  return (
+    <>  
+      <div className='body--container'>
+        <div className='body--title'>
+          Quizzical
+        </div>
+      </div>
+      {
+        isSubmitted === true
+        ?
+        quizResultDisplay
+        :
+        ""
+      }
+      <div className='body--container'>
+        {questionDisplay}
+      </div>
+      {
+        isSubmitted === true
+        ?
+        ""
+        :
+        <div className='body--start-btn' onClick={checkQuiz}> 
+          <p>Check Quiz</p>
+        </div>
+      }
+      <div className='body--container'>
+        <div className='body--footer'>
+          Built and Designed by Georgette Dalen.
+          <br />
+          All rights reserved. ©  
+        </div>
+      </div>
+    </>
+  )
+}
+
